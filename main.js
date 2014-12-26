86bf78fd79397a6c624a84d26883a46e44d24199
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-12-26 16:52:41 +0000

    Now filters my proximity to many reference points

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ var async = require('async'),
 eval(fs.readFileSync('latlon.js') + '');
 eval(fs.readFileSync('gridref.js') + '');
 
-var fetchNearbyPostcodes = function (homeLatLon, maxDistanceKm, callback) {
-	maxDistanceKm = parseFloat(maxDistanceKm);
+var fetchNearbyPostcodes = function (referenceLatLons, maxDistanceMiles, callback) {
+	referenceLatLons = [ ].concat(referenceLatLons); 
+	maxDistanceKm = parseFloat(maxDistanceMiles) * 1.609344;
 	var csv = require('csv');
 	csv()
 		.from.path('data/ONSPD_NOV_2014_csv/Data/ONSPD_NOV_2014_UK.csv', {
@@ -17,15 +18,14 @@ var fetchNearbyPostcodes = function (homeLatLon, maxDistanceKm, callback) {
 			callback(null, data);	
 		})
 		.transform(function (row) {
-			var latLon = OsGridRef.osGridToLatLong(new OsGridRef(row.oseast1m, row.osnrth1m)),
-				// why the h*ll Chris Veness' distanceTo function returns a 
-				// string?
-				distance = parseFloat(latLon.distanceTo(homeLatLon));
-			if (distance <= maxDistanceKm) {
+			var latLon = OsGridRef.osGridToLatLong(new OsGridRef(row.oseast1m, row.osnrth1m));
+			if (_.some(referenceLatLons, function (referenceLatLon) {
+				return parseFloat(latLon.distanceTo(referenceLatLon)) <= maxDistanceKm;
+			})) {
 				row = {
 					'pcds': row.pcds,
 					'lat': latLon.lat(),
-					'lon': latLon.lon(),
+					'lon': latLon.lon()
 				};
 			} else {
 				row = undefined;
@@ -35,6 +35,10 @@ var fetchNearbyPostcodes = function (homeLatLon, maxDistanceKm, callback) {
 }
 
 // home is LatLon(51.759467, -0.577358);
-fetchNearbyPostcodes(new LatLon(51.759467, -0.577358), .5, function (err, postcodes) {
+// Berkhamsted station is LatLon(51.764541, -0.562041);
+fetchNearbyPostcodes([ 
+			new LatLon(51.759467, -0.577358), 
+			// new LatLon(51.764541, -0.562041) 
+		], .1, function (err, postcodes) {
 	console.log(JSON.stringify(postcodes));
 });
