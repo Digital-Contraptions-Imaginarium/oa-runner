27f5b687523d66cb5e93397aefce3502b4af4d93
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-12-26 17:41:37 +0000

    Can now extract lat and lon points of a course from .fit file

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var async = require('async'),
 	_ = require('underscore'),
-	fs = require('fs');
+	fs = require('fs-extra')
+	path = require('path');
 
 eval(fs.readFileSync('latlon.js') + '');
 eval(fs.readFileSync('gridref.js') + '');
@@ -34,11 +35,49 @@ var fetchNearbyPostcodes = function (referenceLatLons, maxDistanceMiles, callbac
 		});
 }
 
+var fetchCourse = function (filename, callback) {
+	var csv = require('csv'),
+		exec = require('child_process').exec,
+		tempFolder = '.' + Math.random().toString(36).substring(7);
+	fs.ensureDirSync(path.join(__dirname, tempFolder));
+	exec(
+		'java -jar etc/FitSDKRelease13.10/java/FitCSVTool.jar -i --data record -b "' + filename + '" "' + path.join(__dirname, tempFolder, 'temp.csv') + '"', 
+		function (err, stdout, stderr) {
+			csv()
+				.from.path(path.join(__dirname, tempFolder, 'temp_data.csv'), {
+					'columns': true,
+					'delimiter': ',',
+				})
+				.to.array(function (data) {
+					fs.removeSync(path.join(__dirname, tempFolder));
+					// remove duplicates (very difficult to find any, with 6 decimal digits)
+					data = _.uniq(data, false, function (x) { return x.lat.toString() + '_' + x.lon.toString(); });
+					callback(null, data);	
+				})
+				.transform(function (row) {
+					// TODO: why does .toFixed return a string?
+					row = {
+						'lat': parseFloat((parseFloat(row["record.position_lat[semicircles]"]) / 11930464.71).toFixed(6)),
+						'lon': parseFloat((parseFloat(row["record.position_long[semicircles]"]) / 11930464.71).toFixed(6))
+					};
+					return row;
+				});
+		}
+	);
+}
+
+
 // home is LatLon(51.759467, -0.577358);
 // Berkhamsted station is LatLon(51.764541, -0.562041);
+/*
 fetchNearbyPostcodes([ 
 			new LatLon(51.759467, -0.577358), 
 			// new LatLon(51.764541, -0.562041) 
 		], .1, function (err, postcodes) {
 	console.log(JSON.stringify(postcodes));
+});
+*/
+
+fetchCourse('data/fit-samples/2014-12-24-11-11-15-Navigate.fit', function (err, data) {
+	console.log(data);
 });
