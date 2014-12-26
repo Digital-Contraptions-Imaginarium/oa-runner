744cfd97e4629419509fd8acee587fb1ffed04f9
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-12-26 16:28:24 +0000

    Filters ONSPD by proximity

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Infer addresses nearby runners' favourite courses, so that they just need to sto
 ##Setup
 - Download the FIT SDK from [http://www.thisisant.com/resources/fit](http://www.thisisant.com/resources/fit) and uncompress it in *etc* (e.g. *etc/FitSDKRelease13.10*).
 - Download the CSV edition of Office for National Statistics' "Postcode Directory" Open Data dataset from [https://geoportal.statistics.gov.uk/geoportal/catalog/content/filelist.page?redirect=Docs/PostCodes/](https://geoportal.statistics.gov.uk/geoportal/catalog/content/filelist.page?redirect=Docs/PostCodes/) and uncompress it in *data* (e.g. *data/ONSPD_NOV_2014_csv*).
+- Download the JSON, one-file-per-postcode-sector edition of Open Addresses UK's addresses-only dataset from [http://alpha.openaddressesuk.org/data](http://alpha.openaddressesuk.org/data) and uncompress it in *data* (e.g. *data/open_addresses_database_2014-12-10-openaddressesuk-addresses-only-split.json*).
+
+## Licence
+Northing/Easting to Latitude/Longitude conversion in JavaScript code is done using Chris Veness' libraries available at [http://www.movable-type.co.uk/scripts/latlong-gridref.html](http://www.movable-type.co.uk/scripts/latlong-gridref.html) licensed under CC-BY 3.0, that implement the algorithms described by Ordnance Survey in the "A guide to coordinate systems in Great Britain" document at [www.ordnancesurvey.co.uk/docs/support/guide-coordinate-systems-great-britain.pdf](www.ordnancesurvey.co.uk/docs/support/guide-coordinate-systems-great-britain.pdf).
--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 var async = require('async'),
 	_ = require('underscore'),
-	fs = require('fs'),
-	parse = require('csv-parse'),
-	transform = require('stream-transform');
+	fs = require('fs');
 
-var output = [],
-	parser = parse({ 'delimiter': ',' }),
-	input = fs.createReadStream('data/ONSPD_NOV_2014_csv/Data/ONSPD_NOV_2014_UK.csv'),
-	transformer = transform(function (record, callback) {
-  		setTimeout(function () {
-    		callback(null, record.join(' ') + '\n');
-  		}, 500);
-	}, {parallel: 10});
-input.pipe(parser).pipe(transformer).pipe(process.stdout);
+eval(fs.readFileSync('latlon.js') + '');
+eval(fs.readFileSync('gridref.js') + '');
+
+var fetchNearbyPostcodes = function (homeLatLon, maxDistanceKm, callback) {
+	maxDistanceKm = parseFloat(maxDistanceKm);
+	var csv = require('csv');
+	csv()
+		.from.path('data/ONSPD_NOV_2014_csv/Data/ONSPD_NOV_2014_UK.csv', {
+			'columns': true,
+			'delimiter': ',',
+		})
+		.to.array(function (data) {
+			callback(null, data);	
+		})
+		.transform(function (row) {
+			var latLon = OsGridRef.osGridToLatLong(new OsGridRef(row.oseast1m, row.osnrth1m)),
+				// why the h*ll Chris Veness' distanceTo function returns a 
+				// string?
+				distance = parseFloat(latLon.distanceTo(homeLatLon));
+			if (distance <= maxDistanceKm) {
+				row = {
+					'pcds': row.pcds,
+					'lat': latLon.lat(),
+					'lon': latLon.lon(),
+				};
+			} else {
+				row = undefined;
+			}
+			return row;
+		});
+}
+
+// home is LatLon(51.759467, -0.577358);
+fetchNearbyPostcodes(new LatLon(51.759467, -0.577358), .5, function (err, postcodes) {
+	console.log(JSON.stringify(postcodes));
+});
